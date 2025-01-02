22667096825c9cf44499e7c1996320d1b9f6edb0
윤다예 <daye.elin.yoon@gmail.com>
2025-01-02 15:28:44 +0900

    주석 내용 수정

--- Plugin/TemplatePlugin/Templates/Feature/Feature.swift
+++ Plugin/TemplatePlugin/Templates/Feature/Feature.swift
@@ -1,10 +1,9 @@
-// Module.swift 파일
 import ProjectDescription
 
-// scaffold 명령어 시 받을 인자.
+// scaffold 명령어 시 받을 인자
 let name: Template.Attribute = .required("name")
 
-// 템플릿 선언.
+// 템플릿 선언
 let moduleTemplate = Template(
     description: "A template for a new feature module",
     attributes: [
@@ -16,7 +15,7 @@ let moduleTemplate = Template(
 enum ModuleTemplate: CaseIterable {
     case view
 
-    // 템플릿 내부에 추가할 파일.
+    // 템플릿 내부에 추가할 파일
     var item: Template.Item {
         switch self {
         case .view:
@@ -25,7 +24,7 @@ enum ModuleTemplate: CaseIterable {
     }
 }
 
-// 템플릿을 추가할 기본 경로.
+// 템플릿을 추가할 기본 경로
 extension String {
     static var basePath: Self {
         return "Projects/\(name)"
